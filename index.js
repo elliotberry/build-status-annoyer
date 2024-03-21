87e692ca607c49c2c1978155f802dd16d1a968a7
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-03-21 14:32:53 -0400

    big changes

--- index.js
+++ index.js
@@ -1,85 +1,27 @@
 import http from 'node:http'
-import playCachedAudio from './lib/playCachedAudio.js'
 import logger from './lib/logger.js'
-import { getVolume, getVoice } from './lib/validation.js'
+import home from './lib/home.js'
+import { doPlay } from './lib/doPlay.js'
+import { logRequestDetails } from './lib/logRequestDetails.js'
 
-var playing = false
-
-const waitForBody = (req) => {
-    return new Promise((resolve, reject) => {
-        let body = ''
-        req.on('data', (chunk) => {
-            body += chunk
-        })
-        req.on('end', () => {
-            resolve(body)
-        })
-    })
-}
-
-const doPlay = async (req, res) => {
+const server = http.createServer(async (req, res) => {
     try {
-        if (playing) {
-            res.writeHead(400, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ error: 'Already playing' }))
-            return
+        // Custom middleware to log request details
+        await logRequestDetails(req)
+
+        if (req.method === 'GET' && req.url === '/') {
+            logger.info('GET /')
+            await home(req, res)
+        } else if (req.method === 'POST' && req.url === '/') {
+            logger.info('POST /')
+            await doPlay(req, res)
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' })
+            res.end('Not Found')
         }
-        let start = Date.now()
-        let body = await waitForBody(req)
-
-        logger.info('body:', body)
-
-        const { message, volume, voice } = JSON.parse(body)
-
-        let volInfo = getVolume(volume)
-        let voiceInfo = getVoice(voice)
-        let allErrors = [volInfo.error, voiceInfo.error].filter(
-            (e) => e === true
-        )
-        if (allErrors.length > 0) {
-            res.writeHead(400, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ error: allErrors.join(', ') }))
-            return
-        }
-
-        playing = true
-        let cached = await playCachedAudio(
-            message,
-            volInfo.volume,
-            voiceInfo.voice
-        ) //if volume
-
-        res.setHeader('Content-Type', 'application/json')
-        logger.info('played in', Date.now() - start, 'ms')
-        let end = Date.now()
-
-        res.end(
-            JSON.stringify({
-                status: 'Sound played',
-                cached,
-                time: end - start,
-            })
-        )
-        playing = false
     } catch (err) {
         res.writeHead(500, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({ error: err.message }))
-    }
-}
-
-const server = http.createServer(async (req, res) => {
-    if (req.method === 'GET' && req.url === '/') {
-        logger.info('GET /')
-        logger.info('hello world')
-        await playCachedAudio('hello world')
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ hello: 'world' }))
-    } else if (req.method === 'POST' && req.url === '/') {
-        logger.info('POST /')
-        await doPlay(req, res)
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' })
-        res.end('Not Found')
     }
 })
 
@@ -91,11 +33,13 @@ server.listen(port, host, () => {
 })
 
 process.on('uncaughtException', (err) => {
-    console.error('Uncaught exception:', err)
+    logger('Uncaught exception:', err)
     process.exit(1)
 })
 
 process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled rejection at:', promise, 'reason:', reason)
+    logger('Unhandled rejection at:', promise, 'reason:', reason)
     process.exit(1)
 })
+
+
